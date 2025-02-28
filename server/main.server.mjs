36ec45b8f3817b31512aa9ac2350f6e6b423bc74
Ricardo Salim <ricardosalim7@hotmail.com>
2025-02-28 04:56:45 -0300

    Auto-generated commit

--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-6DJ762X3.mjs";import"./chunk-YIK4Z4HC.mjs";import"./chunk-EDK7JQSP.mjs";import"./chunk-OI6HREWC.mjs";import"./chunk-FVNYSVNK.mjs";import"./chunk-3GQXTPSB.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
