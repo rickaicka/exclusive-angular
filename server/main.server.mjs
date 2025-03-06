8c37e25678d294e3caacae61d4a227970d6d2b83
rickaicka <rickaicka@users.noreply.github.com>
2025-03-06 22:04:26 +0000

    deploy: ebd6362b6382195be477671ca45e13a91b876573

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-GXT4Q2LB.mjs";import"./chunk-KRDC27EB.mjs";import"./chunk-VF6STBCE.mjs";import"./chunk-ULR6YG3E.mjs";import"./chunk-BRGQIUWT.mjs";import"./chunk-SA4CZCDG.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
